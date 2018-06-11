d3ef11afe2d90714592bf9f95c089f87d39fb814
haison13 <haisonnguyen13@gmail.com>
2018-06-10 22:13:28 -0700

    updated modal login

--- target/classes/public/login/login.js
+++ target/classes/public/login/login.js
@@ -1,10 +1,15 @@
+// small helper for selecting element by id
+let id = id => document.getElementById(id);
 
-// Get the modal
-var modal = document.getElementById('id01');
+id("modal-login").addEventListener("click", show);
+id("close").addEventListener("click", hide);
+id("cancelbtn").addEventListener("click", hide);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+function show(){
+    id("id01").style.display="block";
+}
+
+function hide(){
+    id("id01").style.display="none";
+}
+
